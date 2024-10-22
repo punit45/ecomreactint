1febf6999d93a60b722c96cd058104791c127bb0
punit45 <ipunit96@gmail.com>
2024-10-23 04:24:42 +0530

    half done

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import {ShopContext} from "../context/ShopContext";
+import { assets } from '../assets/assets';
 
 const Collection = () => {
 
@@ -10,7 +11,8 @@ const Collection = () => {
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t-2  '>
         {/* filter */}
         <div className='min-w-60'>
-          <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+          <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+          <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
           {/* category fitler*/}
           <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
             {/* catergires */}
@@ -27,7 +29,25 @@ const Collection = () => {
               </p>
             </div>
           </div>
+          {/* subcategory */}
+          <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}>
+            {/* catergires */}
+            <p className='mb-3 text-sm font-medium'>TYPE</p>
+            <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Topwear'} /> TOPWEAR
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Bottomwear'} /> BOTTOMWEAR
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Winterwear'} /> WINTERWEAR
+              </p>
+            </div>
+          </div>
         </div>
+
+        {/* filter product show */}
     </div>
   )
 }
